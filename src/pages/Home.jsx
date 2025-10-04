a5d579121a8bfcce392fb4a9b38d439c8fa6b54b
IndreshSen <indreshsen100@gmail.com>
2025-10-04 22:58:26 +0530

    new commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import MovieCard from "../components/MovieCard";
+
+function Home(){
+    const movies = [
+        {id:1, title: "john whick " ,release_date: "2020"},
+        {id:1, title: "john whick " ,release_date: "2020"},
+        {id:1, title: "john whick " ,release_date: "2020"},
+    ];
+    const handleSearch = () => {}; 
+    return(
+
+        <div className="home">
+        <form onSubmit={}className="search-form">
+            <input type="text" placeholder="Search from movies..." 
+            className="search-input"/>
+            <button type="submit" className="search-button">Search</button>
+        </form>
+            <div className="movies-grid">
+                {movies.map((movie)=>(
+                <MovieCard movie={movie} key={movie.id}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default Home
