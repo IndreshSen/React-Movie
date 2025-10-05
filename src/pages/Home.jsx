d85e7081797e29e39810cdc19d9adb924284c41b
IndreshSen <indreshsen100@gmail.com>
2025-10-05 20:05:47 +0530

    add nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,34 @@
 import MovieCard from "../components/MovieCard";
+import {useState} from "react"
 
 function Home(){
+    const[searchQuery, setSearchQuery] = useState("");
     const movies = [
         {id:1, title: "john whick " ,release_date: "2020"},
         {id:1, title: "john whick " ,release_date: "2020"},
         {id:1, title: "john whick " ,release_date: "2020"},
     ];
-    const handleSearch = () => {}; 
+    const handleSearch = (e) => {
+        e.preventDefault
+        alert(searchQuery)
+        setSearchQuery("")
+    }; 
     return(
 
         <div className="home">
         <form onSubmit={handleSearch}className="search-form">
             <input type="text" placeholder="Search from movies..." 
-            className="search-input"/>
+            className="search-input"
+                value={searchQuery}
+                onChange={(e)=> setSearchQuery(e.target.value)}
+            />
             <button type="submit" className="search-button">Search</button>
         </form>
             <div className="movies-grid">
-                {movies.map((movie)=>(
-                <MovieCard movie={movie} key={movie.id}/>
+                {movies.map((movie)=> 
+               // movie.title.toLowerCase().startsWith(searchQuery)&&
+               (
+                      <MovieCard movie={movie} key={movie.id}/>
                 ))}
             </div>
         </div>
